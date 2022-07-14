0beec6ee20fc311b0183bf653ba80208e3a75c42
ZHANGLL <2503170029@qq.com>
2022-07-14 19:31:00 +0800

    总项目

---
+++ json-server-api/config.js
@@ -0,0 +1,8 @@
+const config = {
+    //github应用ID
+    client_id: 'c2ca43c8b51298f46cd4',
+    //Github秘钥
+    client_secret: '4644363d170fa9989c85e33ef9ae3364f6a04fab',
+}
+
+module.exports.config = config
